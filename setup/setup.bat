16986aa885956c1514fb806f15fdc18220681a44
Achal <achalddave@live.com>
2012-08-05 12:18:10 -0700

    link .gitignore

--- setup/setup.bat
+++ setup/setup.bat
@@ -3,7 +3,9 @@ if exist "%programfiles(x86)%"\ (
 	mklink "%programfiles(x86)%"\vim\.vimrc "%cd%\..\vimfiles\.vimrc"
 	mklink "%programfiles(x86)%"\vim\.gvimrc "%cd%\..\vimfiles\.gvimrc"
 ) else (
-	mklink "%programfiles%"\vim\.vimrc %cd%\..\vimfiles\.vimrc
-	mklink "%programfiles%"\vim\.gvimrc %cd%\..\vimfiles\.gvimrc
+	mklink "%programfiles%"\vim\.vimrc "%cd%\..\vimfiles\.vimrc"
+	mklink "%programfiles%"\vim\.gvimrc "%cd%\..\vimfiles\.gvimrc"
 )
 
+mklink "%userprofile%"\.gitconfig "%cd%\..\gitfiles\.gitconfig"
+mklink "%userprofile%"\.gitignore_global "%cd%\..\gitfiles\.gitignore_global"
