9138b40d95d8efdd2524e93986e748664791c6e2
Achal <achalddave@live.com>
2012-08-05 11:54:37 -0700

    upates to setup.bat

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,8 +1,5 @@
 @echo off
-REM 64 bit machine
 if exist "%programfiles(x86)%"\ (
-	echo "64 bit"
-	echo %cd%
 	mklink "%programfiles(x86)%"\vim\.vimrc "%cd%\..\vimfiles\.vimrc"
 	mklink "%programfiles(x86)%"\vim\.gvimrc "%cd%\..\vimfiles\.gvimrc"
 ) else (
