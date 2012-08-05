fadb56b2c83714ff4afc9dd8e3036ce50b160c10
Achal <achalddave@live.com>
2012-08-05 11:56:45 -0700

    more setup updates

--- setup/setup.bat
+++ setup/setup.bat
@@ -3,7 +3,6 @@ if exist "%programfiles(x86)%"\ (
 	mklink "%programfiles(x86)%"\vim\.vimrc "%cd%\..\vimfiles\.vimrc"
 	mklink "%programfiles(x86)%"\vim\.gvimrc "%cd%\..\vimfiles\.gvimrc"
 ) else (
-	echo "32 bit"
 	mklink "%programfiles%"\vim\.vimrc %cd%\..\vimfiles\.vimrc
 	mklink "%programfiles%"\vim\.gvimrc %cd%\..\vimfiles\.gvimrc
 )
