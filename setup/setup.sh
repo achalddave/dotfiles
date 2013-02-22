67b5cb06d450a8f684795481d7277210ed75c0f6
Achal <achalddave@live.com>
2013-02-21 16:22:24 -0800

    gitignore global in setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ln -s ${PWD%/*}/gitfiles/.gitconfig ~/.gitconfig
+ln -s ${PWD%/*}/gitfiles/.gitignore_global ~/.gitignore_global
 ln -s ${PWD%/*}/vimfiles/.vimrc ~/.vimrc
 ln -s ${PWD%/*}/vimfiles/.gvimrc ~/.gvimrc
 ln -s ${PWD%/*}/.bashrc ~/.bashrc
