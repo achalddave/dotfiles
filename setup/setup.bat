829a20da88493a78fe651448d6d680644d74d541
Achal <achalddave@live.com>
2013-06-15 22:08:53 -0700

    windows setup fix

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal enableextensions
 
-mklink "%userprofile%"\.vimrc "%cd%\..\vimfiles\.vimrc"
-mklink "%userprofile%"\.gvimrc "%cd%\..\vimfiles\.gvimrc"
+mklink /H "%userprofile%"\.vimrc "%cd%\..\vimfiles\.vimrc"
+mklink /H "%userprofile%"\.gvimrc "%cd%\..\vimfiles\.gvimrc"
 
-mklink "%userprofile%"\.gitconfig "%cd%\..\gitfiles\.gitconfig"
-mklink "%userprofile%"\.gitignore_global "%cd%\..\gitfiles\.gitignore_global"
-mklink "%userprofile%"\.inputrc "%cd%\..\.inputrc"
-mklink "%userprofile%"\.bashrc "%cd%\..\.bashrc"
-mklink "%userprofile%"\.bashrc_local "%cd%\..\local\.bashrc"
-mklink "%userprofile%"\.inputrc "%cd%\..\.inputrc"
+mklink /H "%userprofile%"\.gitconfig "%cd%\..\gitfiles\.gitconfig"
+mklink /H "%userprofile%"\.gitignore_global "%cd%\..\gitfiles\.gitignore_global"
+mklink /H "%userprofile%"\.inputrc "%cd%\..\.inputrc"
+mklink /H "%userprofile%"\.bashrc "%cd%\..\.bashrc"
+mklink /H "%userprofile%"\.bashrc_local "%cd%\..\local\.bashrc"
+mklink /H "%userprofile%"\.inputrc "%cd%\..\.inputrc"
 REM mklink "%userprofile%"\Documents\WindowsPowershell\Microsoft.Powershell_profile.ps1 "%cd%"\..\powershell\profile.ps1
