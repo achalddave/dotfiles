3bc049b965500eece829c3a4c1c92f081e2903fe
Achal Dave <achalddave@live.com>
2013-07-20 19:28:31 -0700

    create *_local files if necessary

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,3 +6,9 @@ ln -s ${PWD%/*}/vimfiles/.vimrc ~/.vimrc
 ln -s ${PWD%/*}/vimfiles/.gvimrc ~/.gvimrc
 ln -s ${PWD%/*}/.bashrc ~/.bashrc
 ln -s ${PWD%/*}/.inputrc ~/.inputrc
+
+# create ~/.bashrc_local, otherwise ~/.bashrc will complain when sourcing it
+# if it exists and we touch, we'd change modification time
+if [ ! -e ~/.bashrc_local ] ; then
+    touch ~/.bashrc_local
+fi
--- setup/setup_zsh.sh
+++ setup/setup_zsh.sh
@@ -2,3 +2,9 @@
 
 ln -s ${PWD%/*}/zsh/scripts ~/.zsh_scripts
 ln -s ${PWD%/*}/zsh/.zshrc ~/.zshrc
+
+# create ~/.zshrc_local, otherwise ~/.zshrc will complain when sourcing it
+# if it exists and we touch, we'd change modification time
+if [ ! -e ~/.zshrc_local ] ; then
+    touch ~/.zshrc_local
+fi
