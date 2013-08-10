f6735723a7b8cbb1720276072926002046d69f94
Achal Dave <achalddave@live.com>
2013-08-10 14:55:41 -0700

    cleanup setup.bat

--- setup/setup.bat
+++ setup/setup.bat
@@ -8,6 +8,4 @@ mklink /H "%userprofile%"\.gitconfig "%cd%\..\gitfiles\.gitconfig"
 mklink /H "%userprofile%"\.gitignore_global "%cd%\..\gitfiles\.gitignore_global"
 mklink /H "%userprofile%"\.inputrc "%cd%\..\.inputrc"
 mklink /H "%userprofile%"\.bashrc "%cd%\..\.bashrc"
-mklink /H "%userprofile%"\.bashrc_local "%cd%\..\local\.bashrc"
-mklink /H "%userprofile%"\.inputrc "%cd%\..\.inputrc"
 REM mklink "%userprofile%"\Documents\WindowsPowershell\Microsoft.Powershell_profile.ps1 "%cd%"\..\powershell\profile.ps1
