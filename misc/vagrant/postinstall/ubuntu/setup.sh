f7ed15613fe49b45eecb9528b4e0d72fc8531466
Achal Dave <achalddave@live.com>
2014-01-24 20:11:58 +0000

    add basic vagrant config

---
+++ misc/vagrant/postinstall/ubuntu/setup.sh
@@ -0,0 +1,7 @@
+if [ ! -e ~/dircolors ] ; then
+    # Windows folders show up with an ugly green background because they're
+    # other writable; fix this
+    dircolors -p | sed 's/OTHER_WRITABLE 34;42/OTHER_WRITABLE 01;34/' > ~/dircolors
+else
+    echo "Dircolors already exists! Not changing."
+fi
---
+++ misc/vagrant/postinstall/ubuntu/tools.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install git
