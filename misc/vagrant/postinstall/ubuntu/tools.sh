b3c12d391a7a481ae2aa10c4c29822142e8e31b1
Achal Dave <achalddave@live.com>
2014-01-24 23:29:56 +0000

    vagrant postinstall: use latest version of git

--- misc/vagrant/postinstall/ubuntu/tools.sh
+++ misc/vagrant/postinstall/ubuntu/tools.sh
@@ -1,3 +1,11 @@
 sudo apt-get update
-sudo apt-get install vim
+
+# For add-apt-repository (http://askubuntu.com/questions/38021/how-to-add-a-ppa-on-a-server)
+sudo apt-get install python-software-properties
+
+# Add repo for latest Ubuntu Git version
+sudo add-apt-repository ppa:git-core/ppak
+sudo apt-get update
+
 sudo apt-get install git
+sudo apt-get install vim
