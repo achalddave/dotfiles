3422ec5ba8f382978b741d9acbc722687a2ff1b0
Achal Dave <achalddave@live.com>
2014-01-26 01:53:20 +0000

    vagrant: create tools folder, install zsh, --yes for apt-get, python install

---
+++ misc/vagrant/postinstall/ubuntu/python.sh
@@ -0,0 +1,3 @@
+sudo apt-get --yes install python3
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+sudo python get-pip.py
--- misc/vagrant/postinstall/ubuntu/tools/main.sh
+++ misc/vagrant/postinstall/ubuntu/tools/main.sh
@@ -1,11 +1,13 @@
 sudo apt-get update
 
 # For add-apt-repository (http://askubuntu.com/questions/38021/how-to-add-a-ppa-on-a-server)
-sudo apt-get install python-software-properties
+sudo apt-get --yes install python-software-properties
 
 # Add repo for latest Ubuntu Git version
 sudo add-apt-repository ppa:git-core/ppak
 sudo apt-get update
 
-sudo apt-get install git
-sudo apt-get install vim
+sudo apt-get --yes install git
+sudo apt-get --yes install vim
+sudo apt-get --yes install make
+sudo apt-get --yes install zsh
