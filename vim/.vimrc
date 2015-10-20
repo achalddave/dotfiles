achalddave/dotfiles

commit d112e598af0a9fba87707eb90880b02411b0e3ee

vim: Don't allow h/l in visual mode to go across lines

vim/.vimrc
@@ -415,7 +415,7 @@ if has('mouse')
 endif
 
 " Wrap cursor
-set ww+=<,>,[,]
+set ww+=[,]
 
 " stop highlighting the current line if not active
 au WinLeave * set nocursorline

commit a249cf77dd8ce54853451808cd289053047f4798

vim: Allow <Left>,<Right> in visual/normal mode to cross lines
This reverts d112e598af0a9fba87707eb90880b02411b0e3ee, which mistakenly
thought that the whichwrap issue, which allowed <h> and <l> to cross
lines in visual/normal mode, was caused by my "set ww" line; in reality,
it was caused by the Pencil plugin, which was removed by
9889fdde94acb660834eb0f5f11bacbf5200636c.

vim/.vimrc
@@ -397,7 +397,7 @@ if has('mouse')
 endif
 
 " Wrap cursor
-set ww+=[,]
+set ww+=<,>,[,]
 
 " stop highlighting the current line if not active
 au WinLeave * set nocursorline