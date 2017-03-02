85458434391144aa40101ba9f97c4ec47c975438
Achal Dave <achalddave@live.com>
2017-03-02 12:00:26 -0500

    zsh: Print code used to load json in python helper

--- zsh/scripts/pyjson_helper.py
+++ zsh/scripts/pyjson_helper.py
@@ -1,13 +1,19 @@
 import argparse
 import json
+import inspect
 
 parser = argparse.ArgumentParser()
 parser.add_argument('json_file')
 args = parser.parse_args()
 
+def load_json(json_file):
+    with open(json_file) as f:
+        return json.load(f)
+
 if __name__ == '__main__':
-    with open(args.json_file) as f:
-        data = json.load(f)
-        print '\n###'
-        print 'Loaded JSON in variable `data`.'
-        print '###'
+    data = load_json(args.json_file)
+    print '\n'
+    print inspect.getsource(load_json)
+    print '\n###'
+    print 'Loaded JSON in variable `data`.'
+    print '###'
