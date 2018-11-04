d20f425f861a359ea51e9286069432532a8474ad
Achal Dave <achalddave@live.com>
2018-11-04 18:09:26 -0500

    zsh: pyjson - Return instead of quitting shell on error

--- zsh/scripts/pyjson.zsh
+++ zsh/scripts/pyjson.zsh
@@ -3,7 +3,7 @@ function pyjson() {
     # Launches ipython with JSON file loaded as the variable "data".
     if [[ "$#" != 1 ]] ; then
         echo "Usage: $0 <json_file>"
-        exit 1
+        return
     fi
     ipython -i ${script_dir}/pyjson_helper.py "$1"
 }
