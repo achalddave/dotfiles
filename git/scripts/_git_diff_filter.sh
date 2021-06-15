0d3c6a136aa8a0b5bc1ba464873dcbe92c82e74a
Achal Dave <achalddave@live.com>
2021-06-15 19:15:38 +0000

    Add a comment about diff-highlight

--- git/scripts/_git_diff_filter.sh
+++ git/scripts/_git_diff_filter.sh
@@ -1,9 +1,14 @@
 if type delta >/dev/null 2>&1 ; then
     delta --color-only --theme="zenburn"
 else
+    # 6/15/21: diff-highlight has been removed from the diff-so-fancy repo, so
+    # the below needs to be fixed. You could use diff-so-fancy --patch as
+    # indicated here: https://github.com/so-fancy/diff-so-fancy/issues/401
+    # but I was not able to get it to work currently.
+
+    # Earlier note:
     # diff-so-fancy has some issues with git add --patch. For example, it shows some
     # lines from the next or previous hunks. See
     # https://github.com/so-fancy/diff-so-fancy/pull/172 .
-    # diff-so-fancy
     diff-highlight | less --tabs=4 -RFX
 fi
