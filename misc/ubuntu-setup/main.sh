e2d5f26c906211c17fb2e58d406173e70a87d275
Achal Dave <achalddave@live.com>
2023-03-02 13:55:05 -0800

    Add fzf, ripgrep, fd to ubuntu setup

--- misc/ubuntu-setup/main.sh
+++ misc/ubuntu-setup/main.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 
+
 # For add-apt-repository (http://askubuntu.com/questions/38021/how-to-add-a-ppa-on-a-server)
 sudo apt-get --yes install python-software-properties
 
@@ -16,6 +17,15 @@ sudo apt-get --yes install zsh
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
+
+sudo apt-get install ripgrep
+sudo apt-get install fd-find
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+mkdir -p ~/.local/bin/
+ln -s $(which fdfind) ~/.local/bin/fd
 
 touch ~/.zshenv
 echo 'DEBIAN_PREVENT_KEYBOARD_CHANGES=yes' >> ~/.zshenv
